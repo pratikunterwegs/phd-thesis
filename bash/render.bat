59caf68a6ec70ad8f97a06e080b28d768dabc01e
Pratik Gupte <pratikgupte16@gmail.com>
2022-04-07 11:13:48 +0200

    updates

---
+++ bash/render.bat
@@ -0,0 +1,9 @@
+@REM !/bin/bash
+
+@REM  to render from tex to pdf via lualatex
+
+del main.pdf
+
+lualatex -interaction=nonstopmode main.tex
+biber main.bcf
+lualatex -interaction=nonstopmode main.tex
